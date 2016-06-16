8567b032d708a4913788874152cf97768631ec49
yasch107 <tendalor.alb@hotmail.de>
2016-06-16 11:32:58 +0200

    Added kill-button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,15 @@
 package de.hhu.propra16;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+
+    @FXML
+    protected void handlekillbutton(ActionEvent kill){
+        System.exit(0);
+    }
 }
