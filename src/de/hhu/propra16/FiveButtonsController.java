b3145b1366654546879192b5675d9de2928251bc
L0RCH <lukas.tf.lang@gmail.com>
2016-06-16 11:28:22 +0200

    Aufgabe e erledigt

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,21 @@
 package de.hhu.propra16;
 
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+    @FXML
+    Text time;
+
+    @FXML
+    void time(ActionEvent event){
+        AnimationTimer timer = new TimeAction(time);
+        timer.start();
+    }
 }
