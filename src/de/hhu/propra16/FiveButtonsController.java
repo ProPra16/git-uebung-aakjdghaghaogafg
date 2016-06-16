000c5ed6b83246fdbd471a416a7a842511b1d438
MiWer106 <michellewerth@hotmail.com>
2016-06-16 11:46:31 +0200

    add Alert Button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,16 @@
 package de.hhu.propra16;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+    @FXML
+    Text alert;
+    public void handleButton(){
+        alert.setVisible(true);
+    }
 }
